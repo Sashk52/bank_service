84a4ccb6620bc7e78d73962fef989d75e2b425f1
Sashk52 <a.tsven@gmail.com>
2021-02-28 23:10:21 +0200

    try solve checkstyle problem

--- src/main/java/com/example/bank/service/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bank.service.demo.repository;
-
-import com.example.bank.service.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
