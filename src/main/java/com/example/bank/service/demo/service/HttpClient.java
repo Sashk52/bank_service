8631a440474711c5197163d7f6e76afc654de576
Sashk52 <a.tsven@gmail.com>
2021-03-11 21:50:54 +0200

    added ClientService & HttpClient

---
+++ src/main/java/com/example/bank/service/demo/service/HttpClient.java
@@ -0,0 +1,27 @@
+package com.example.bank.service.demo.service;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpClient {
+    public BigDecimal sendHttpRequest(String path) {
+        try {
+            URL url = new URL(path);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+            JsonElement root = JsonParser
+                    .parseReader(new InputStreamReader((InputStream) request.getContent()));
+            return root.getAsJsonObject().get("result").getAsBigDecimal();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't parse object" + e);
+        }
+    }
+}
